64f60f648b118aa8a110da4db701491e618d5960
Katya <ater.93@mail.ru>
2026-01-21 10:12:14 +0300

    add RemindersScreen.kt

--- app/src/main/java/com/example/serials/MainActivity.kt
+++ app/src/main/java/com/example/serials/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.serials
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.example.serials.ui.navigation.Navigation
 import com.example.serials.ui.theme.SerialsTheme
 import com.example.serials.ui.viewmodel.SerialsViewModel
@@ -15,6 +17,7 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var viewModel: SerialsViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "🎬 Activity создается")
